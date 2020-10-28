9af5b88b7da1b810857e32d5c01230670d59ac87
Alexander <banderulez@ya.ru>
2020-10-28 21:00:20 +0300

    added homework 3

--- students/Khenerin Alexander/project/script.js
+++ students/Khenerin Alexander/project/script.js
@@ -19,15 +19,39 @@
 // };
 //
 // render(goods);
+
+function addBasket(id) {
+    cart.addToBasket(id);
+};
+
+function deleteItem(id) {
+    cart.deleteFromBasket(id);
+};
+
+function viewCart() {
+    cart.render();
+};
+const API_URL = './json/response.json';
+    function makeGETRequest(url, callback) {
+    return new Promise((resolve, reject) => {
+        let xhr = window.XMLHttpRequest ? new window.XMLHttpRequest() : new window.ActiveXObject;
+        xhr.open("GET", url, true);
+        xhr.onload = () => resolve(callback(xhr.responseText));
+        xhr.onerror = () => reject(xhr.statusText);
+        xhr.send();
+    });
+}
+
 class GoodItem {
-    constructor(title = 'Товар', price = 'Цена', img = '#') {
+    constructor(id, title, price, img) {
+        this.id = id;
         this.title = title;
         this.price = price;
         this.img = img;
     }
 
     render() {
-        return `<div class="goods-item"><div class="goods-info"><img src="${this.img}" alt="${this.title}"><h3>${this.title}</h3><p>${this.price}</p></div><button class='addClick'>Добавить</button></div>`;
+        return `<div class="goods-item"><div class="goods-info"><img src="${this.img}" alt="${this.title}"><h3>${this.title}</h3><p>${this.price}</p></div><button class='addClick' onclick='addBasket(${this.id})'>Добавить</button></div>`;
     }
 }
 
@@ -36,13 +60,12 @@ class GoodsList {
         this.goods = [];
     }
 
-    fetchGoods() {
-        this.goods = [
-            { title: 'Мышка', price: 500, img: "./img/мышь.jpg"},
-            { title: 'Ноутбук', price: 50000, img: './img/ноутбук.jpg'},
-            { title: 'Клавиатура', price: 5000, img: './img/клавиатура.jpg'},
-            { title: 'Монитор', price: 10000, img: './img/монитор.jpg'},
-        ];
+    fetchGoods(url) {
+        makeGETRequest($API_URL, (good) => {
+            this.goods = JSON.parse(good);
+            this.render();
+            this.calcAllGoods();
+        })
     }
 
     render() {
@@ -69,50 +92,84 @@ class GoodsList {
 
 
 class BasketItem {
-
-    constructor(title, price, img, link) {
+    constructor(id, title, price, img) {
+        this.id = id;
         this.title = title;
         this.price = price;
         this.img = img;
-        this.link = link;
     }
-
     render() {
-
+        return `<div class="basket-item"><img src="${this.img}" alt="${this.title}"><div class="basket-info"><h3>${this.title}</h3><p>${this.price}</p></div><button class='deleteItem' onclick='deleteItem(${this.id})'>&times;</button></div>`;
     }
 }
+
 
 class Basket {
     constructor() {
+        this.cartGoods = [];
 
-        this.addGoods = [];
-        this.deletedGoods = [];
+    addToBasket(id) {
+        let toBasket;
+        list.goods.forEach(function(item) {
+            if(id == item.id) {
+                toBasket = {
+                    id: item.id,
+                    title: item.title,
+                    price: item.price,
+                    img: item.img
+                }
+            }
+        });
+        this.cartGoods.push(toBasket);
+        this.basketCount();
     }
 
-/
 
-    addToBasket() {
+    deleteFromBasket(id) {
+        let getIdElemen;
+        this.cartGoods.forEach(function(item, i) {
+            let thisId = item.id;
+            if(id == thisId) {
+                getIdElemen = i;
+            }
+
+        });
+        this.cartGoods.splice(getIdElemen, 1);
+        this.render();
+        this.basketCount();
     }
 
-    deleteFromBasket() {
+
+    calcAllGoods() {
+        let totalPrice = 0;
+        this.cartGoods.forEach((good) => {
+            if (good.price !== undefined) {
+                totalPrice += good.price;
+            }
+        });
+        let totalGoodsAnswer = "Общая сумма товаров в корзине: $" + totalPrice;
+        document.querySelector('.goods-total').innerHTML = totalGoodsAnswer;
     }
 
-    calcBasket() {
+
+    basketCount() {
+        let count = this.cartGoods.length;
+        document.getElementById('cartcoint').innerHTML = ' (' + count + ')';
     }
 
-    isOrder() {
-    }
 
     render() {
-    }
-
-    openBasket() {
+        let readHtml = '';
+        this.cartGoods.forEach((good) => {
+            const goodItem = new BasketItem(good.id, good.title, good.price, good.img);
+            readHtml += goodItem.render();
+        })
+        document.querySelector('.goods-list').innerHTML = readHtml;
+        this.calcAllGoods();
     }
 }
 
+
 const list = new GoodsList();
-list.fetchGoods();
-window.onload = () => {
-    list.render();
-    list.calcAllGoods();
-};
+const cart = new Basket();
+list.fetchGoods('response.json');
